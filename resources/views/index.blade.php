9168ff7388a63783cef8d1bc9da0e4bd56ab8acd
Andre Costa <andre2396costa@gmail.com>
2024-12-05 15:21:28 -0300

    criando component input

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.base>
     <div class="flex flex-row items-center justify-center w-full">
-        <x-button
+        {{-- <x-button
             type="button"
             text="Enviar"
             variation="primary"
@@ -8,6 +8,17 @@
             icon="home"
             iconPosition="left"
         >
-        </x-button>
+        </x-button> --}}
+
+        <x-form.input 
+            type="text"
+            name="name"
+            placeholder="name"
+            size="large"
+            icon="home"
+            iconPosition="right"
+            label="Digite seu nome"
+        />
+
     </div>
 </x-layouts.base>
